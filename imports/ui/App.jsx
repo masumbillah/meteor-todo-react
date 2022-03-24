a7a6c99dbdabbbcf65d691e6a19fb67d48591830
Masum Billah <masum.billah78@gmail.com>
2022-03-24 15:36:37 +0600

    first commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Hello } from './Hello.jsx';
+import { Info } from './Info.jsx';
+
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+    <Hello/>
+    <Info/>
+      <i><h1>App build by Masum Billah</h1></i>
+  </div>
+);
